4ee3334bc0980abc077c94b5102113660e06d904
LIU Xiao <liu.xiao.88@foxmail.com>
2022-08-12 13:55:57 +0800

    2.3. Adding a Progress Indicator

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
     for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -19,7 +20,9 @@ fn main() {
             let ig = (255.999 * g) as u8;
             let ib = (255.999 * b) as u8;
 
-            println!("{} {} {}", ir, ig, ib);
+            print!("{} {} {}\n", ir, ig, ib);
         }
     }
+
+    eprint!("\nDone.\n");
 }
