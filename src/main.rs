6f5347b517f3da3c8fc16de769c50b4d4ccd7739
LIU Xiao <liu.xiao.88@foxmail.com>
2022-08-16 09:53:35 +0800

    RT In One Weekend - 5. Adding a Sphere

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use crate::color::{Color, write_color};
 use crate::ray::Ray;
-use crate::vec3::{Float, Point3, unit_vector, Vec3};
+use crate::vec3::{Float, Point3, unit_vector, Vec3, dot};
 
 mod color;
 mod ray;
 mod vec3;
 
+fn hit_sphere(center: Point3, radius: Float, r: Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = dot(r.direction(), r.direction());
+    let b = 2.0 * dot(oc, r.direction());
+    let c = dot(oc, oc) - radius * radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant > 0.0
+}
+
 fn ray_color(r: Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
