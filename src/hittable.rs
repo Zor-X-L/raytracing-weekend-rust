38bd015f987a0b2259d823c92cb4f6e3ba6cdba5
LIU Xiao <liu.xiao.88@foxmail.com>
2022-08-16 16:52:15 +0800

    RT In One Weekend - 6. Surface Normals and Multiple Objects - 6.5

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,11 +1,19 @@
 use crate::float::Float;
 use crate::ray::Ray;
-use crate::vec3::{Point3, Vec3};
+use crate::vec3::{Point3, Vec3, dot};
 
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: Float,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        self.front_face = dot(r.direction(), *outward_normal) < 0.0;
+        self.normal = if self.front_face { *outward_normal } else { -*outward_normal };
+    }
 }
 
 pub trait Hittable {
