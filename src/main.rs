cbb5cd14c12d036dbf4009feafa69f0d9125cedf
LIU Xiao <liu.xiao.88@foxmail.com>
2022-08-16 12:30:52 +0800

    RT In One Weekend - 6. Surface Normals and Multiple Objects - 6.1

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,24 @@ mod color;
 mod ray;
 mod vec3;
 
-fn hit_sphere(center: Point3, radius: Float, r: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: Float, r: &Ray) -> Float {
     let oc = r.origin() - center;
     let a = dot(r.direction(), r.direction());
     let b = 2.0 * dot(oc, r.direction());
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r);
+    if t > 0.0 {
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
     let unit_direction = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
